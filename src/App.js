8d02e4ed93d8a1cdb886e4e65707ff45ae635b0a
nikthewanted <77376892+nikthewanted@users.noreply.github.com>
2023-11-05 05:46:06 +0530

    just api of harry potter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from "react";
 
 function App() {
+  const [value,setValue]=useState()
+  
+  async function data(){
+    const response=await fetch('https://hp-api.onrender.com/api/characters/students')
+    const json=await response.json([])
+    console.log(json)
+    setValue(json)
+  }
+  useEffect (()=>{
+    data()
+  }
+
+  ,[])
+
+  const myimg = {
+    height: '200px',
+    width: '200px'
+  }
+
+  const cont = {
+    display:'flex',
+    flexDirection:'rows',
+    backgroundColor:'lightgreen',
+  
+
+  }
+  const outer ={
+    display:'grid',
+    gap:'20px 20px',
+    backgroundColor:'yellow'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={cont}>
+      <div style={outer}>   
+          {value ?   value.map((item)=>(
+            <>
+            <h1 key={item.name}>Name of Charcter is: {item.name}</h1>
+            <img src={item.image} style={myimg}/> 
+            <h1>Charcter Played by :{item.actor}</h1>
+            
+            <br/>
+
+            </>
+            
+          ))
+          : null}
+      </div>
     </div>
   );
 }
